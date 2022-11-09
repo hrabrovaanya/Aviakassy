ceee0f15bd758ccd9cf044726a319758da0a50f9
Anna Khr <94216329+hrabrovaanya@users.noreply.github.com>
2022-11-09 17:24:58 +0000

    Произведен рефакторинг кода.

--- main.cs
+++ main.cs
@@ -13,7 +13,19 @@ using System.IO;
         {
             Console.Write("Добро пожаловать в авиакассу \"Freedom flight\".\n(Мы не отвечаем за реальное существование предлагаемых рейсов, вы всё делаете на свой страх и риск).\n");
             Console.Write("Здесь вы можете преобрести билеты по направлениям:\n");
+            preview_information();
+            Console.Write("Введите пункт прибытия: ");
+            string place_of_arrival = Console.ReadLine();
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            List<Flight_list> flight_list = new List<Flight_list>();//list of searched flights
+            flight_list = Build_a_list_of_flights(flight_list, place_of_arrival);
+            Displaying_a_list_of_flights(flight_list);
+            Console.ReadLine();
+        }
 
+
+        static void preview_information()//displays information about available flights
+        {
             string line = "";
             using (StreamReader s = new StreamReader("File//Имеющиеся рейсы.txt"))
             {
@@ -23,98 +35,121 @@ using System.IO;
                     Console.Write($"Тула - {l[0]}\n");
                 }
             };
-            Console.Write("Введите пункт прибытия: ");
-            string where = Console.ReadLine();
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            List<Avia> av = new List<Avia>();
-            av = BuildAviaList(av, where);
-            Vivod(av);
-            Console.ReadLine();
         }
-        static List<Avia> BuildAviaList(List<Avia> av, string where)
+
+
+        static List<Flight_list> Build_a_list_of_flights(List<Flight_list> flight_list, string place_of_arrival)//Creates a list with all the information about the searched flights
         {
             string line = "";
+            bool flight_availability = false;
             using (StreamReader s = new StreamReader("File//Имеющиеся рейсы.txt"))
             {
                 while ((line = s.ReadLine()) != null)
                 {
+                    
                     string[] l = line.Split('|');
-                    if (l[0] == where) {
-                        av.Add(new Avia(l[0], l[1]));
+                    if (l[0] == place_of_arrival)
+                    {
+                        flight_list.Add(new Flight_list(l[0], l[1]));
+                        flight_availability = true;
                     }
                 }
             };
-            return av;
+            if (!flight_availability) { Console.Write("У нас не продаются билеты на данный рейс...\n"); }
+            return flight_list;
         }
-        static void Vivod(List<Avia> av)
+
+
+        static void Displaying_a_list_of_flights(List<Flight_list> flight_list)//display of all information about the desired flights
         {
-            foreach (var list_1 in av)
+            foreach (var list_1 in flight_list)
             {
-                Console.WriteLine($"Рейсы по направлению: Тула - {list_1.where}");
+                Console.WriteLine($"Рейсы по направлению: Тула - {list_1.Place_of_arrival}");
                 Console.WriteLine("-----------------------------------------------------------------------------------");
-                foreach (var list_2 in list_1.p_t)
+                foreach (var list_2 in list_1.Flight_information1)
                 {
-                    Console.WriteLine($"Рейс: {list_2.flight}, \nТип самолета: {list_2.type}, \nДата и время отлета: {list_2.takeoff_time}, \nДата и время прилета: {list_2.landing_time}");
-                    foreach (var list_3 in list_2.crew)
-                      {
-                        Console.WriteLine($"Экипаж: \n\tПилот: {list_3.pilot},  \n\tВторой пилот: {list_3.co_pilot}, \n\tБортовой инженер: {list_3.engineer},  \n\tСостав бортпроводников: {list_3.stewar}");
+                    Console.WriteLine($"Рейс: {list_2.Flight_number}, \nТип самолета: {list_2.Flight_type}, \nДата и время отлета: {list_2.Takeoff_time}, \nДата и время прилета: {list_2.Landing_time}");
+                    foreach (var list_3 in list_2.Crew1)
+                    {
+                        Console.WriteLine($"Экипаж: \n\tПилот: {list_3.Pilot},  \n\tВторой пилот: {list_3.Co_pilot}, \n\tБортовой инженер: {list_3.Engineer},  \n\tСостав бортпроводников: {list_3.Stewar}");
                     }
                     Console.WriteLine("Билеты в продаже:");
-                    foreach (var list_4 in list_2.tickets)
+                    foreach (var list_4 in list_2.Tickets)
                     {
-                        Console.WriteLine($"\tКласс: {list_4.clas},  \n\tКоличество: {list_4.quantity},  \n\tЦена: {list_4.price}\n");
+                        Console.WriteLine($"\tКласс: {list_4.Flight_class},  \n\tКоличество: {list_4.Quantity},  \n\tЦена: {list_4.Price}\n");
                     }
                     Console.WriteLine("-----------------------------------------------------------------------------------");
                 }
             }
         }
     }
-    class Avia
+
+
+    class Flight_list //contains all the information about the searched flights
     {
-        public string where;
-        public string id;
-        public List<Place_Time> p_t= new List<Place_Time>();
-        public Avia(string where, string id)//Страна прибытия
+        private string place_of_arrival;
+        private string id;
+        private List<Flight_information> flight_information1 = new List<Flight_information>();
+
+        public string Place_of_arrival { get => place_of_arrival; set => place_of_arrival = value; }
+        public string Id { get => id; set => id = value; }
+        internal List<Flight_information> Flight_information1 { get => flight_information1; set => flight_information1 = value; }
+
+        public Flight_list(string place_of_arrival, string id)
         {
-            this.where = where;
-            this.id = id;
-            this.p_t = Place_Time_Add(this.id,p_t);
+            this.Place_of_arrival = place_of_arrival;
+            this.Id = id;
+            this.Flight_information1 = Flight_information_Add(this.Id, Flight_information1);
         }
-        private List<Place_Time> Place_Time_Add(string id, List<Place_Time> p_t)
+
+
+        private List<Flight_information> Flight_information_Add(string id, List<Flight_information> flight_information)
         {
-          string line = "";
+            string line = "";
             using (StreamReader s = new StreamReader("File//Информация о рейсе.txt"))
             {
                 while ((line = s.ReadLine()) != null)
                 {
                     string[] l = line.Split('|');
-                    if (id == l[0]) {
-                        p_t.Add(new Place_Time(l[1], l[2], l[3], l[4]));
+                    if (id == l[0])
+                    {
+                        flight_information.Add(new Flight_information(l[1], l[2], l[3], l[4]));
                     }
                 }
             };
 
-            return p_t;
+            return flight_information;
         }
-        public class Place_Time //Информация по рейсу
+
+
+        public class Flight_information //class containing all information about flights
         {
-            public string flight;
-            public string type;
-            public string takeoff_time;
-            public string landing_time;
-           public List<Crew> crew = new List<Crew>();
-           public List<Ticket> tickets = new List<Ticket>();
- 
-            public Place_Time(string flight, string type, string takeoff_time, string landing_time)
+            private string flight_number;
+            private string flight_type;
+            private string takeoff_time;
+            private string landing_time;
+            private List<Crew> crew1 = new List<Crew>();
+            private List<Ticket> tickets = new List<Ticket>();
+
+            public string Flight_number { get => flight_number; set => flight_number = value; }
+            public string Flight_type { get => flight_type; set => flight_type = value; }
+            public string Takeoff_time { get => takeoff_time; set => takeoff_time = value; }
+            public string Landing_time { get => landing_time; set => landing_time = value; }
+            internal List<Crew> Crew1 { get => crew1; set => crew1 = value; }
+            internal List<Ticket> Tickets { get => tickets; set => tickets = value; }
+
+            public Flight_information(string flight_number, string flight_type, string takeoff_time, string landing_time)
             {
-                this.flight = flight;
-                this.type = type;
-                this.takeoff_time = takeoff_time;
-                this.landing_time = landing_time;
-                this.crew = Crew_Add(this.flight, crew);
-                this.tickets = Ticket_Add(this.flight, tickets);
+                this.Flight_number = flight_number;
+                this.Flight_type = flight_type;
+                this.Takeoff_time = takeoff_time;
+                this.Landing_time = landing_time;
+                this.Crew1 = Сreate_crew_list(this.Flight_number, Crew1);
+                this.Tickets = Сreate_ticket_list(this.Flight_number, Tickets);
             }
-            private List<Crew> Crew_Add(string flight, List<Crew> crew)
+
+
+            private List<Crew> Сreate_crew_list(string flight_number, List<Crew> crew)
             {
                 string line = "";
                 using (StreamReader s = new StreamReader("File//Экипаж.txt"))
@@ -122,7 +157,7 @@ using System.IO;
                     while ((line = s.ReadLine()) != null)
                     {
                         string[] l = line.Split('|');
-                        if (flight == l[0])
+                        if (flight_number == l[0])
                         {
                             crew.Add(new Crew(l[1], l[2], l[3], l[4]));
                         }
@@ -131,7 +166,9 @@ using System.IO;
 
                 return crew;
             }
-            private List<Ticket> Ticket_Add(string flight, List<Ticket> tickets)
+
+
+            private List<Ticket> Сreate_ticket_list(string flight_number, List<Ticket> tickets)
             {
                 string line = "";
                 using (StreamReader s = new StreamReader("File//Билеты.txt"))
@@ -139,46 +176,59 @@ using System.IO;
                     while ((line = s.ReadLine()) != null)
                     {
                         string[] l = line.Split('|');
-                        if (flight == l[0])
+                        if (flight_number == l[0])
                         {
-                           tickets.Add(new Ticket( l[1], Int32.Parse(l[2]), Int32.Parse(l[3])));
+                            tickets.Add(new Ticket(l[1], Int32.Parse(l[2]), Int32.Parse(l[3])));
                         }
                     }
                 };
 
                 return tickets;
             }
-           public class Crew //Экипаж
+
+
+            public class Crew //a class containing information about the crew of the flight
             {
-                public string pilot;
-                public string co_pilot;
-                public string engineer;
-                public string stewar;
+                private string pilot;
+                private string co_pilot;
+                private string engineer;
+                private string stewar;
+
+                public string Pilot { get => pilot; set => pilot = value; }
+                public string Co_pilot { get => co_pilot; set => co_pilot = value; }
+                public string Engineer { get => engineer; set => engineer = value; }
+                public string Stewar { get => stewar; set => stewar = value; }
 
                 public Crew(string pilot, string co_pilot, string engineer, string stewar)
                 {
-                    this.pilot = pilot;
-                    this.co_pilot = co_pilot;
-                    this.engineer = engineer;
-                    this.stewar = stewar;
+                    this.Pilot = pilot;
+                    this.Co_pilot = co_pilot;
+                    this.Engineer = engineer;
+                    this.Stewar = stewar;
 
                 }
-             
+
             }
-            public class Ticket //Билеты
+
+
+            public class Ticket //class containing information about flight tickets
             {
-                public string clas;
-                public int quantity;
-                public int price;
+                private string flight_class;
+                private int quantity;
+                private int price;
 
-                public Ticket(string clas, int quantity, int price)
+                public string Flight_class { get => flight_class; set => flight_class = value; }
+                public int Quantity { get => quantity; set => quantity = value; }
+                public int Price { get => price; set => price = value; }
+
+                public Ticket(string flight_class, int quantity, int price)
                 {
-                    this.clas = clas;
-                    this.quantity = quantity;
-                    this.price = price;
+                    this.Flight_class = flight_class;
+                    this.Quantity = quantity;
+                    this.Price = price;
                 }
             }
-      
+
         }
     }
-   
+
